9d1019e2c4aad8116cb246289df7726675de262d
Vitor Hugo <VitorHugoOli@github.com>
2019-10-16 14:17:04 -0300

    Configuraçoes finais do mongo

--- treatmentFile/models.py
+++ treatmentFile/models.py
@@ -1,10 +1,10 @@
 from djongo import models
 
+
 # Create your models here.
 class Texto(models.Model):
     titulo = models.CharField(max_length=200)
     texto = models.CharField(max_length=1000000)
 
-
     def __str__(self):
-        return  self.titulo
+        return self.titulo
