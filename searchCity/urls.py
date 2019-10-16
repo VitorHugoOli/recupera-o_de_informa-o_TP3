afb4778450cc096186ed2b35ad29222bc50b2aa0
Vitor Hugo <VitorHugoOli@github.com>
2019-10-16 01:07:24 -0300

    Atualizando nome

--- searchCity/urls.py
+++ searchCity/urls.py
@@ -1,4 +1,4 @@
-"""Back_end_TP3 URL Configuration
+"""searchCity URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
