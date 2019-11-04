cc2c62c12ea6cc95475f0ea1cd3c54b087990267
vitorhugooli <vho2009@hotmail.com>
2019-11-04 03:29:10 -0200

    Adicinando o titulo as palavras de procura

--- frontend/src/pages/Search/index.js
+++ frontend/src/pages/Search/index.js
@@ -1,19 +1,16 @@
 import React, {useState} from 'react';
-import axios from 'axios';
 import api from '../../services/api'
 
 
 import search from '../../assets/search.png';
 
-
 export default function Search({history}){
     const [termo, setTermo] = useState('');
 
     async function handleSubmit(){
+        const result = await api.get('/search/belo')
 
-        const result = await axios.post('http://127.0.0.1:8000/search',{"teste":"teste"})
-
-        console.log(result)
+        console.log(result.data)
 
         history.push('/results');
     }
